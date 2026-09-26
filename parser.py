b6592d7d65c9e9f0b6fe6c55a91d7196ed6002f9
shusmihoqe-233009042 <shusmi.hoqe@icloud.com>
2026-09-26 15:14:52 +0600

    Create parser.py

---
+++ parser.py
@@ -0,0 +1,6 @@
+while_statement
+    → WHILE LPAREN expression RPAREN statement
+switch_statement
+    → SWITCH LPAREN expression RPAREN LBRACE case_list RBRACE
+array_declaration
+    → ARRAY IDENTIFIER LBRACKET NUMBER RBRACKET
